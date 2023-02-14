2ac9734d65e51fd43a5ad0e5020dca982bf59f1b
Chandra Mohan Ray <cmohan932@hotmail.com>
2023-02-15 01:05:42 +0530

    Added studentTester

---
+++ src/LenghthOfArray.java
@@ -0,0 +1,11 @@
+public class LenghthOfArray {
+
+        public static void main(String[] args) {
+            System.out.println(args.length);    // length of the array
+            System.out.println(args[0]);
+            System.out.println(args[1]);
+        }
+    }
+
+
+
---
+++ src/StudentTester.java
@@ -0,0 +1,11 @@
+public class StudentTester{
+
+    public static void main(String[] args) {
+
+
+
+        //Create an object of Student class
+        //Use setter methods to set the values
+        //Use getter methods with proper escape sequences to display values
+    }
+}
